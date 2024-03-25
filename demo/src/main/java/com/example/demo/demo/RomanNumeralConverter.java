4cfd021207d79516402e41acc2c4b1aa43d5b178
Joe <JoeRabitt@AOL.com>
2024-03-25 15:25:47 +0100

    Anpassung 3 Methode

--- demo/src/main/java/com/example/demo/demo/RomanNumeralConverter.java
+++ demo/src/main/java/com/example/demo/demo/RomanNumeralConverter.java
@@ -3,8 +3,8 @@ package com.example.demo.demo;
 public class RomanNumeralConverter {
 
     public static String convertToRoman(int number) {
-        String[] romanNumerals = {"V", "IV", "I"};
-        int[] values = {5, 4, 1};
+        String[] romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         StringBuilder roman = new StringBuilder();
     
         for (int i = 0; i < values.length; i++) {
