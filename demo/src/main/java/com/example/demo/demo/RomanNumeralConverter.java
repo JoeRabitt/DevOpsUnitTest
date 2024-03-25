0826ca7d804780b43c5f60af1c4dde37df2f15ae
Joe <JoeRabitt@AOL.com>
2024-03-25 15:00:35 +0100

    Anapssung 1 Methode

--- demo/src/main/java/com/example/demo/demo/RomanNumeralConverter.java
+++ demo/src/main/java/com/example/demo/demo/RomanNumeralConverter.java
@@ -3,8 +3,8 @@ package com.example.demo.demo;
 public class RomanNumeralConverter {
 
     public static String convertToRoman(int number) {
-        String[] romanNumerals = {"I"};
-        int[] values = {1};
+        String[] romanNumerals = {"I", "IV"};
+        int[] values = {1, 4};
         StringBuilder roman = new StringBuilder();
     
         for (int i = 0; i < values.length; i++) {
